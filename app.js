c6cdf45138103c1d63cf96f1a8a5befe7b46f7b3
Azka Adhisetama <adhisetama@gmail.com>
2022-09-20 17:20:12 +0700

    backend: edit data obat dari database

--- app.js
+++ app.js
@@ -10,15 +10,12 @@ app.use(express.static('public'))
 const { Medicine, medicineLog, logEnum } = require('./utils/db')
 const { ObjectId } = require('mongodb')
 
+// halaman index
 app.get('/', async (req, res) => {
     const medicines = await Medicine.find() 
     res.render('index', {
         medicines
     })
-})
-
-app.listen(PORT, () => {
-    console.log('listening to port ' + PORT)
 })
 
 //tambah data obat ke database
@@ -31,4 +28,48 @@ app.post('/add', async (req, res) => {
     )]
     await Medicine.insertMany(req.body)
     res.redirect('/')
+})
+
+// hapus data obat dari database
+app.get('/delete/:id', async (req, res) => {
+    const medicine = await Medicine.findOne({_id: ObjectId(req.params.id)})
+    if (medicine) {
+        await Medicine.deleteOne({_id: ObjectId(req.params.id)})
+        res.redirect('/')
+    } else {
+        res.status(404)
+        res.send('<h1>Error 404: Not Found</h1>')
+    }
+})
+
+// edit data obat dari database
+app.get('/edit/:id', async (req, res) => {
+    const medicine = await Medicine.findOne({_id: ObjectId(req.params.id)})
+    if (medicine) {
+        res.render('edit', { medicine })
+    } else {
+        res.status(404)
+        res.send('<h1>Error 404: Not Found</h1>')
+    }
+})
+app.post('/edit', async (req, res) => {
+    const medicine = await Medicine.findOne({_id: ObjectId(req.body._id)})
+    if (medicine) {
+        await Medicine.updateOne(
+            {_id: ObjectId(req.body._id)},
+            {
+                $set: {
+                    name: req.body.name,
+                    manufacturer: req.body.manufacturer,
+                    description: req.body.description
+                }
+            }
+        )
+        res.redirect('/')
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log('listening to port ' + PORT)
 })
