dcb0cf5a61953905d0cc83f2387ae8f8f15acf8c
irhar <irharsytara02@gmail.com>
2022-09-20 17:30:20 +0700

    menambahkan transaction

--- app.js
+++ app.js
@@ -69,6 +69,36 @@ app.post('/edit', async (req, res) => {
     }
 })
 
+// edit data transaksi obat
+app.get('/transaction/:id', async (req, res) => {
+    const medicine = await Medicine.findOne({_id: ObjectId(req.params.id)})
+    if (medicine) {
+        res.render('transaction', {
+            medicine
+        })
+    }
+})
+app.post('/transaction', async (req, res) => {
+    const medicine = await Medicine.findOne({_id: ObjectId(req.body._id)})
+    if (medicine) {
+        const newLog = medicine.log
+        newLog.push(medicineLog(
+            req.body.stock, req.body.description
+        ))
+        const newStock = medicine.stock + parseInt(req.body.stock)
+        await Medicine.updateOne(
+            {_id: ObjectId(req.body._id)},
+            {
+                $set: {
+                    log: newLog,
+                    stock: newStock
+                }
+            }
+        )
+        res.redirect(`/info/${medicine._id}`)
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
