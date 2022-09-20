f16a5e794069bb51f147c057b3cd1b4a1e2b20fe
clstyn <fiorenzacelestyn@gmail.com>
2022-09-20 17:09:14 +0700

    fix: add medicine

--- app.js
+++ app.js
@@ -6,14 +6,29 @@ app.use(express.urlencoded())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-require('./utils/db')
-require('mongodb')
+// koneksi dengan database
+const { Medicine, medicineLog, logEnum } = require('./utils/db')
+const { ObjectId } = require('mongodb')
 
 app.get('/', async (req, res) => {
-    
-    res.render('index')
+    const medicines = await Medicine.find() 
+    res.render('index', {
+        medicines
+    })
 })
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
+})
+
+//tambah data obat ke database
+app.get('/add', async (req, res) => {
+    res.render('add')
+})
+app.post('/add', async (req, res) => {
+    req.body.log = [medicineLog(
+        req.body.stock, "Data obat ditambahkan ke database"
+    )]
+    await Medicine.insertMany(req.body)
+    res.redirect('/')
 })
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://ananta:ananta@atlascluster.mbhpn8l.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://admin:admin@testmongo.rbvgcyd.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
