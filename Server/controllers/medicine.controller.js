1f74bba068bba0d6ec05a734fb4d06cbdfea5cba
Adhisetama <adhisetama@gmail.com>
2022-09-29 00:45:28 +0700

    fix some bug, change some thing, add /:id/log route

--- Server/controllers/medicine.controller.js
+++ Server/controllers/medicine.controller.js
@@ -1,136 +1,190 @@
-// const db = require("../models/index");
-const { Medicine } = require("../models/medicine");
-// const Medicines = db.medicines;
+const db = require('../models')
+const Medicine = db.medicines.Medicine
+const medicineLog = db.medicines.medicineLog
 
-// create
-exports.create = (req, res) => {
-    if (!req.body.name) {
-        res.status(400).send({ message: "Content can not be empty!" });
-        return;
-    }
+module.exports = {
 
-    const medicine = new Medicine({
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        stock: req.body.stock
-    });
-    
-    medicine
-        .save(medicine)
-        .then(data => {
-        res.send(data);
-        })
-        .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while adding the medicine."
-        });
-        });
-};
-
-exports.findAll = (req, res) => {
-    const id = req.query.id;
-    var condition = id ? { id: { $regex: new RegExp(id), $options: "i" } } : {};
-    
-    Medicine.find(condition)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tutorials."
-        });
-      });
-};
-
-exports.findOne = (req, res) => {
-    const id = req.params.id;
-  
-    Medicine.findById(id)
-      .then(data => {
-        if (!data)
-          res.status(404).send({ message: "Not found Tutorial with name " + id });
-        else res.send(data);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .send({ message: "Error retrieving Tutorial with id=" + id });
-      });
-};
-
-exports.update = (req, res) => {
-    if (!req.body) {
-      return res.status(400).send({
-        message: "Data to update can not be empty!"
-      });
-    }
-  
-    const id = req.params.id;
-  
-    Medicine.findByIDAndUpdate(id, req.body, { useFindAndModify: false })
-      .then(data => {
-        if (!data) {
-          res.status(404).send({
-            message: `Cannot update Tutorial with name=${id}. Maybe Tutorial was not found!`
-          });
-        } else res.send({ message: "Tutorial was updated successfully." });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Tutorial with id=" + id
-        });
-      });
-};
-
-  exports.delete = (req, res) => {
-    const id = req.params.id;
-  
-    Medicine.findByIdAndDelete(id)
-      .then(data => {
-        if (!data) {
-          res.status(404).send({
-            message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
-          });
+    /* .post /add, JSON { name, manufacturer, description, stock } */
+    create(req, res) {
+        if (!(req.body.name 
+            && req.body.manufacturer
+            && req.body.description
+            && req.body.stock)) {
+            res.status(400).send({
+                message: "request's body must contain 'name', 'manufacturer', 'description', and 'stock' field"
+            })
         } else {
-          res.send({
-            message: "Tutorial was deleted successfully!"
-          });
+        const medicine = new Medicine({
+            name: req.body.name,
+            manufacturer: req.body.manufacturer,
+            description: req.body.description,
+            stock: req.body.stock,
+            log: [medicineLog(
+                req.body.stock,
+                'data obat ditambahkan'
+            )]
+        })
+        medicine.save()
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                    err.message || 'Error creating medicine.'
+                })
+            })
         }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete Tutorial with name = " + id
-        });
-      });
-  };
+    },
 
-  exports.deleteAll = (req, res) => {
-    Medicine.deleteMany({})
-      .then(data => {
-        res.send({
-          message: `${data.deletedCount} Tutorials were deleted successfully!`
-        });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all tutorials."
-        });
-      });
-  };
+    /* .get /, /?id=<id>&name=<name>&manufacturer=<manufacturer>
+    optional, jika req.query kosong, send semua isi database */
+    findAll(req, res) {
+        const query = {}
 
-  exports.findAllPublished = (req, res) => {
-    Medicine.find({ published: true })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tutorials."
-        });
-      });
-  };
+        if (req.query.name)
+            query.name = {
+                $regex: new RegExp(req.query.name),
+                $options: "i"
+            }
+        if (req.query.manufacturer)
+            query.manufacturer =  {
+                $regex: new RegExp(req.query.manufacturer),
+                $options: "i"
+            }
+
+        Medicine.find(query)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                    err.message || "Some error occured while retreiving medicines"
+                })
+            })
+
+    },
+
+    /* .get /:id */
+    findOne(req, res) {
+        const id = req.params.id
+
+        Medicine.findById(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({
+                        message: `Medicine with id ${id} not found`
+                    })
+                } else res.send(data)
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: `Error retreiving medicine with id ${id}`
+                })
+            })
+    },
+
+    /* .put /:id JSON { ... }  */
+    update(req, res) {
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).send({
+                message: "Data to update can not be empty!"
+            })
+        }
+        // res.send(req.body)
+        const id = req.params.id
+
+        Medicine.findByIdAndUpdate(id, { $set: req.body }, { useFindAndModify: false })
+            .then(data => {
+                if (!data)
+                    res.status(404).send({
+                        message: `Medicine with id ${id} not found`
+                    })
+                else res.send({ message: "medicine was updated successfully" })
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                        err.message || `Error updating medicine with id ${id}`
+                })
+            })
+    },
+
+    /* .put /:id/log JSON { stock, description } */
+    addTransactionLog(req, res) {
+        const id = req.params.id
+        let log, newLog
+
+        if ('stock' in req.body) {
+
+            newLog = [
+                req.body.stock,
+                req.body.description
+                    ? req.body.description
+                    : '',
+                new Date().toLocaleString()
+            ]
+
+            Medicine.findById(id).then(data => {
+                log = data.log
+                log.push(newLog)
+
+                Medicine.findByIdAndUpdate(id, { $set: { log }})
+                .then(data => {
+                    if (!data)
+                        res.status(404).send({
+                            message: `Medicine with id ${id} not found`
+                        })
+                    else res.send({ message: `Transaction log added for medicine with id ${id}` })
+                })
+
+            }).catch(() => {
+                res.status(404).send({
+                    message: `Medicine with id ${id} not found`
+                })
+            })
+
+        } else return res.status(400).send({
+            message: "cannot add empty transaction log"
+        })
+
+        // log.push(newLog)
+        // res.send({log, newLog})
+
+        // Medicine.findByIdAndUpdate(id, { $set: { log }})
+        //     .then(data => {
+        //         if (!data)
+        //             res.status(404).send({
+        //                 message: `Medicine with id ${id} not found`
+        //             })
+        //         else res.send({ message: `Transaction log added for medicine with id ${id}` })
+        //     })
+
+    },
+
+    /* .delete /:id  */
+    delete(req, res) {
+        const id = req.params.id
+
+        Medicine.findByIdAndDelete(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({
+                    message: `Cannot delete medicine with id=${id}`
+                    })
+                } else {
+                    res.send({
+                    message: `Medicine with id ${id} was deleted successfully!`
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: `Could not delete medicine with id ${id}`
+                })
+            })
+    }
+    
+
+}
--- Server/server.js
+++ Server/server.js
@@ -4,13 +4,8 @@ const cors = require('cors')
 const app = express()
 
 var corsOptions = {
-    origin: "http://localhost:5000"
+    origin: "http://localhost:9000"
 }
-
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to pharmaweb" });
-});
 
 // middlewares
 app.use(cors(corsOptions))
@@ -18,8 +13,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 require("./routes/medicine.routes")(app)
-// ^^ note: belum dibuat, tambah medicine.controller, diimport ke
-// medicine.routes, medicine.routes di import kesini
 
 // set port, listen for requests
 const PORT = process.env.PORT || 9000
@@ -29,6 +22,5 @@ console.log(`Server is running on port ${PORT}.`)
 
 
 // database connection
-// const db = require('./models')
-const connectDB = require('./config/db').connectDB
-connectDB()
+const db = require('./models')
+db.connectDB()
