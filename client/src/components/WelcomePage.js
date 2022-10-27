40772fe539d2a63b1a11eb29efd570783805390b
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2022-10-27 20:36:45 +0700

    feat: Setup tailwind for css

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const WelcomePage = () => {
   return (
     <div>
-        <h2>Selamat datang di</h2>
+        <h2 className="text-3xl font-bold underline text-red-500">Selamat datang di</h2>
         <h1>Pharma-Web</h1>
     </div>
   )
