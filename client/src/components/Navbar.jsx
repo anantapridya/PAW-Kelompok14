079d41748b36671f654dea9f736d0290cf48cf06
Adhisetama <adhisetama@gmail.com>
2022-10-27 23:55:21 +0700

    add Navbar.jsx component, create css & img folder inside src

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from '../img/websitelogo.png'
+import '../css/Navbar.css'
+
+// height: 64px
+export default function Navbar() {
+    return (
+        <nav role="navigation">
+        <ul className="nav--list">
+            <li className="nav--logo">
+                <img src={logo} alt="app logo on navigation bar"></img>
+                <span>Pharma</span><span>Web</span>
+            </li>
+            <li><a href="/">List</a></li>
+            <li><a href="/add">Add</a></li>
+            <li><a href="#">Sign in/Sign out</a></li>
+        </ul>
+    </nav>
+    )
+}
+
+// note to do:
+// add href to sign in/sign out
