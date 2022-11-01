5b733cf4ad1386789aa36e754a1f9de6fbf1ca1c
irhar <irharsytara02@gmail.com>
2022-11-01 20:44:31 +0700

    add addmedicine page

--- client/src/pages/AddMedicine.js
+++ client/src/pages/AddMedicine.js
@@ -1,7 +1,62 @@
+import DefaultBtn from "../components/DefaultBtn";
+import DefaultInput from "../components/DefaultInput";
+import Navbar from "../components/Navbar";
+
 const AddMedicine = () => {
   return (
-    <div>
-      <h1>Tambah Obat</h1>
+    <div className="bg-putih md:h-screen">
+      <Navbar/>
+      <div className="font-body mx-[150px] my-[30px] text-xl">
+        <h1 className="text-biru-sedang font-heading font-bold text-4xl">
+          Tambah Obat
+        </h1>
+
+        <p>Nama Obat :</p>
+        {/* TODO: Buat sejajar */}
+        <div className="inline-block">
+          <DefaultInput
+            placeholder="Paracetamol"
+            className="w-[330px] h-[50px]"
+          />
+        </div>
+
+        <p>Manufacturer Obat :</p>
+        {/* TODO: Buat sejajar */}
+        <div className="inline-block">
+          <DefaultInput
+            placeholder="PT Sindo Farma"
+            className="w-[330px] h-[50px]"
+          />
+        </div>
+
+        <p>Stok Obat :</p>
+        {/* TODO: Buat sejajar */}
+        <div className="inline-block">
+          <DefaultInput
+            placeholder="1000"
+            className="w-[330px] h-[50px]"
+          />
+          <button>+</button>
+          <button>-</button>
+        </div>
+
+        {/* TODO: Buat jadi paragraf multiline */}
+        <p>Deskripsi Obat :</p>
+        <DefaultInput
+          type="textarea"
+          placeholder="Tambahkan deskripsi mengenai obat"
+          className="w-full h-[200px]"
+        />
+
+        {/* TODO: Buat ke tengah */}
+        <button>Batal</button>
+
+        <DefaultBtn
+          type="button"
+          judulButton="Tambah Obat"
+          className=""
+        />
+      </div>
     </div>
   )
 };
