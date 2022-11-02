bafcc1d7ad78a1a3d85f841e7075215e43d8016a
anantapridya <ananta20pridya@gmail.com>
2022-11-02 21:45:28 +0700

    Add Modal Component

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,91 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+export default function Modal({desc, btn, btnmodal}) {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className="">
+        <button
+          type="button"
+          onClick={openModal}
+          className="font-body text-[20px] px-[50px] py-[5px] bg-[#FF0000] text-white rounded-[8px] mr-[15px] "
+        >
+          {btn}
+        </button>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className=" px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Message Box
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500 border-t pt-2">
+                    {desc}
+                  </p>
+                </div>
+
+                <div className="mt-4 flex flex-row-reverse">
+                  <button
+                    type="button"
+                    className=" px-4 py-2 text-sm text-white bg-[#FF0000] border border-transparent rounded-md hover:bg-red-200 duration-300"
+                    onClick={closeModal}
+                  >
+                    {btnmodal}
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
