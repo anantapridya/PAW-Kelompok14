1e2f3369f33347eef685efc8beb2b24e10dedabd
clstyn <fiorenzacelestyn@gmail.com>
2022-11-04 08:40:14 +0700

    feat(backend): connect auth and medicine route

--- Server/routes/medicine.routes.js
+++ Server/routes/medicine.routes.js
@@ -1,19 +1,22 @@
-    module.exports = app => {
+const { authJwt } = require("../middleware");
+
+module.exports = app => {
     const medicine = require("../controllers/medicine.controller.js");
     
     const router = require("express").Router();
     
-    router.post("/add", medicine.create); 
+    router.post("/add", [authJwt.verifyToken, authJwt.isAdmin], medicine.create); 
   
-    router.get("/", medicine.findAll);
+    router.get("/", [authJwt.verifyToken], medicine.findAll);
   
-    router.get("/:id", medicine.findOne);
-    router.put("/:id", medicine.update);
-    router.delete("/:id", medicine.delete);
-    router.delete("/", medicine.deleteAll);
+    router.get("/:id", [authJwt.verifyToken], medicine.findOne);
 
-    router.get("/:id/log", medicine.getTransactionLog)
-    router.put("/:id/log", medicine.addTransactionLog)
+    router.put("/:id", [authJwt.verifyToken,  authJwt.isAdmin], medicine.update);
+    router.delete("/:id", [authJwt.verifyToken,  authJwt.isAdmin], medicine.delete);
+    router.delete("/", [authJwt.verifyToken,  authJwt.isAdmin], medicine.deleteAll);
+
+    router.get("/:id/log", [authJwt.verifyToken,  authJwt.isAdmin], medicine.getTransactionLog)
+    router.put("/:id/log", [authJwt.verifyToken,  authJwt.isAdmin], medicine.addTransactionLog)
 
     router.get("/:id/stock", medicine.getMedicineStock)
   
