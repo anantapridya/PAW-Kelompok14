3bd3f4dae7d209bd6c138c387b2bd1945d4a4f49
clstyn <fiorenzacelestyn@gmail.com>
2022-11-05 20:52:32 +0700

    style (med desc): change css to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,10 +16,20 @@ module.exports = {
         'putih': '#ECFCFF'
       },
       boxShadow: {
-        '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25)'
+        '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        '4xl': '-3px 25px 25px 5px rgba(63,101,255,0.1)',
+        '5xl': '0 0 10px 5px rgba(63,101,255,0.2)',
+        '6xl': '5px 8px 25px 5px rgba(63,101,255,0.1)'
       },
       dropShadow: {
-        '3xl': '0px 0px 5px rgba(63, 101, 255, 0.4)'
+        '3xl': '0px 0px 5px rgba(63, 101, 255, 0.4)',
+        '4xl': '2px 3px 2px rgba(63, 101, 255, .4)'
+      },
+      gridTemplateColumns: {
+        'desc-page': '1fr 600px'
+      },
+      gridTemplateRows: {
+        'desc-page': '1fr'
       }
     },
   },
