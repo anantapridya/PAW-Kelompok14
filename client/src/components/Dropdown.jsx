79d78eddef9651bb374086f57047bd6c5ab403a8
Ananta Pridya <ananta20pridya@gmail.com>
2022-11-07 18:34:55 +0700

    fix: Dropdown component

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,12 +1,12 @@
 import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 
-export default function Dropdown({ Sort }) {
+export default function Dropdown(props) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center rounded-md border-2 border-[#3F65FF] bg-white px-4 h-[36px] items-center text-[16px] font-medium text-black font-poppins hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-          Sort
+        <Menu.Button className={"inline-flex w-full justify-center rounded-md bg-white px-4 h-[36px] items-center text-[16px] font-medium font-body text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 " + props.className}>
+          {props.judul}
         </Menu.Button>
       </div>
       <Transition
@@ -20,52 +20,24 @@ export default function Dropdown({ Sort }) {
       >
         <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? "bg-[#3F65FF] text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  onClick={Sort.A_to_Z}
-                >
-                  A-Z
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? "bg-[#3F65FF] text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  onClick={Sort.Z_to_A}
-                >
-                  Z-A
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? "bg-[#3F65FF] text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  Harga Terendah
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? "bg-[#3F65FF] text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  Harga Tertinggi
-                </button>
-              )}
-            </Menu.Item>
+            {props.items.map((data) => {
+              const { id, value } = data;
+              return (
+                <div key={id}>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-[#3F65FF] text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        {value}
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              );
+            })}
           </div>
         </Menu.Items>
       </Transition>
