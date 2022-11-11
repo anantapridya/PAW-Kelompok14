ec0bbf0f6e7e061c8136e8838030d33541abd369
Ananta Pridya <ananta20pridya@gmail.com>
2022-11-11 17:35:01 +0700

    Configure navbar items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
 import logo from '../img/websitelogo.png'
+import React, {useState} from 'react'
+import Dropdown from './Dropdown'
 // import '../css/Navbar.css'
 
-export default function Navbar() {
+export default function Navbar({isLogin}) {
+    const profile =[{id:1, value:"Profile"},{id:2, value:"Sign Out"}]
     return (
-        <nav role="navigation" className="h-[60px] w-full px-[60px] box-border bg-putih font-body font-medium text-[20px] text-biru-tua xl:h-16 xl:text-[22px]">
+    <nav role="navigation" className="h-[60px] w-full px-[60px] box-border bg-putih font-body font-medium text-[20px] text-biru-tua xl:h-16 xl:text-[22px]">
         <ul className="h-full w-full flex items-center box-border list-none">
             <li className='mr-[50px] mx-[30px] xl:mx-[50px]'>
                 <Link to="/" className="flex items-center hover:drop-shadow-3xl">
@@ -12,9 +15,10 @@ export default function Navbar() {
                     <span className='font-bold text-[23px] xl:text-[25px] ml-[13px]'>Pharma</span><span className='font-bold text-[23px] xl:text-[25px] text-black'>Web</span>
                 </Link>
             </li>
-            <li className="mx-[50px] xl:mx-[30px] hover:drop-shadow-3xl"><Link to="/list">List</Link></li>
-            <li className="mx-[50px] xl:mx-[30px] hover:drop-shadow-3xl"><Link to="/add">Add</Link></li>
-            <li className="mx-[50px] ml-auto hover:drop-shadow-3xl"><Link to="/welcome">Sign in/Sign out</Link></li>
+            <li className={`mx-[50px] xl:mx-[30px] hover:drop-shadow-3xl ${isLogin ? "hidden": "inline"}`}><Link to="/list">List</Link></li>
+            <li className={`mx-[50px] xl:mx-[30px] hover:drop-shadow-3xl ${isLogin ? "hidden": "inline"}`}><Link to="/add">Add</Link></li>
+            <li className={`mx-[50px] ml-auto hover:drop-shadow-3xl ${isLogin ? "inline": "hidden"}`}><Link to="/welcome">Sign in/Sign out</Link></li>
+            <li className={`mx-[50px] ml-auto hover:drop-shadow-3xl ${isLogin ? "hidden": "inline"} `}><Dropdown items={profile} judul="Profile" className="text-biru-tua text-[22px] bg-transparent" /></li>
         </ul>
     </nav>
     )
