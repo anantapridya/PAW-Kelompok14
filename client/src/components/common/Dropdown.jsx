543cb57b92eb0322314381fb25c45efc1b5f5320
Ipung Sonia F <79226247+ipungsonia@users.noreply.github.com>
2022-11-24 09:56:50 +0700

    style: fix position dropdown
    add z-index to make dropdown in front

--- client/src/components/common/Dropdown.jsx
+++ client/src/components/common/Dropdown.jsx
@@ -18,7 +18,7 @@ export default function Dropdown(props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 mt-2 w-56 z-50 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             {props.items.map((data) => {
               const { id, value } = data;
